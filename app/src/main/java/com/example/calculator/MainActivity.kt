74fc78821a811bee766d17e28be058b2099c1cdb
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2025-02-12 23:11:19 +0100

    chore: initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.calculator.ui.theme.CalculatorTheme
+import com.example.calculator.ui.theme.MediumGray
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculatorTheme {
+                val viewModel: CalculatorViewModel by viewModels()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+
+                Calculator(
+                    state = state,
+                    buttonSpacing = buttonSpacing,
+                    onAction = viewModel::onAction,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.DarkGray)
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
